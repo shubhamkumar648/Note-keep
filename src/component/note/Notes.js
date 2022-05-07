9aca80c9c0b26888b51ea84c688417a8ace7e966
shubhamkumar648 <shubhamwork96@gmail.com>
2022-05-08 01:30:40 +0530

    feat: add color pallet functionality

--- src/component/note/Notes.js
+++ src/component/note/Notes.js
@@ -6,32 +6,47 @@ import { useAuth } from "../../contexts/Auth-context";
 import { useNotes } from "../../contexts/Notes-context";
 import { setNoteReducer } from "../../reducer/setNoteReducer";
 import "./notes.css";
+import { v4 as uuidv4 } from 'uuid';
+
+
+const NoteColors = [
+  {id: uuidv4(), color: ""},
+  {id: uuidv4() , color: "teal"},
+  {id: uuidv4(), color: "blue"},
+  {id: uuidv4(), color: "red"},
+  {id: uuidv4(), color: "brown"},
+  {id: uuidv4(), color: "green"},
+
+]
 
 export const Notes = () => {
-  const {  notesDispatch } = useNotes();
-
+  const { notesDispatch } = useNotes();
   const { encodedToken } = useAuth();
-
+  
   const [state, dispatch] = useReducer(setNoteReducer, {
     title: "",
     textarea: "",
+    noteColor:"",
+    isColorpalletVisible: false
   });
 
-  const { title, textarea } = state;
+const { title, textarea,noteColor,isColorpalletVisible } = state;
 
-  const handleClick = async (e) => {
+
+const saveNotesHandler = async (e) => {
     e.preventDefault();
-
+   
     let addNote = {
       title,
       textarea,
+      noteColor
     };
 
     try {
       const response = await axios.post(
         "/api/notes",
         { note: addNote },
-       
+
         {
           headers: {
             authorization: encodedToken,
@@ -43,14 +58,14 @@ export const Notes = () => {
         dispatch({ type: "RESET" });
       }
     } 
-    
     catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className="main-text-container flex flex-col mt-3">
+    <div className="main-text-container  flex flex-col mt-3">
+    <form className={`note-color-${noteColor}`}onSubmit= {saveNotesHandler}>
       <input
         type="text"
         placeholder="Title"
@@ -71,17 +86,30 @@ export const Notes = () => {
         }
       ></textarea>
       <section className="text-footer flex">
-        <div className="icons_container">
-          <FaPalette />
+        <div className="icons_container flex">
+         <span onClick={() => dispatch({type:"COLOR_PALLET"})}> <FaPalette /> </span>
           <FaTag className="ml-1" />
         </div>
+              {
+                isColorpalletVisible && <div className="colorpallete_container"> 
+                     {NoteColors.map(({id,color}) =>(
+                     
+                     <div key = {id} className={`note-colorBox note-color-${color}`}
+                       onClick={() => dispatch({action:"SET_COLOR",payload:color})}
+                     > 
+                      </div>
+                     )
+                     )}
+                </div>
+              }
 
         <div className="addButton flex">
-          <button onClick={handleClick} className="btn addButton">
+          <button type="submit" className="btn addButton">
             Add
           </button>
         </div>
       </section>
+      </form>
     </div>
   );
 };
--- src/component/notecard/Notecard.js
+++ src/component/notecard/Notecard.js
@@ -1,34 +1,28 @@
-import React from 'react'
-import "./notecard.css"
-import { FaArchive,FaTrash,FaEdit, FaThumbtack,  } from "react-icons/fa";
+import React from "react";
+import "./notecard.css";
+import { FaArchive, FaTrash, FaEdit, FaThumbtack } from "react-icons/fa";
 
-export const Notecard = ({noteContent}) => {
-
-const {title,textarea} = noteContent
+export const Notecard = ({ noteContent }) => {
+  const { title, textarea } = noteContent;
 
   return (
     <div>
-    <div className="main-noteCard-container flex flex-col ">
-       
-     <div className='NoteCard_Header flex'>
-      <p>{title}</p>
-      <FaThumbtack/>
-     </div>
-          
+      <div className="main-noteCard-container flex flex-col ">
+        <div className="NoteCard_Header flex">
+          <p>{title}</p>
+          <FaThumbtack />
+        </div>
 
-   <p>{textarea}</p>
+        <p>{textarea}</p>
 
-       <section className="cart-footer flex">
-       <p>Create at</p>
-       <FaEdit/>
+        <section className="cart-footer flex">
+          <p>Create at</p>
+          <FaEdit />
 
-       <FaArchive/>
-       <FaTrash/>  
-
-
-       </section>
-     
-   </div>
+          <FaArchive />
+          <FaTrash />
+        </section>
+      </div>
     </div>
-  )
-}
+  );
+};
